38f966c66042319bc886afb9f3e279bcab2dcc53
GottaGinger <114538296+GottaGinger@users.noreply.github.com>
2023-09-13 21:12:22 -0400

    update
    SQL connection

---
+++ gardenwhispererAPI/Models/Plant.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gardenwhispererAPI.Models;
+
+public partial class Plant
+{
+    public int Id { get; set; }
+
+    public int? UserId { get; set; }
+
+    public string ScienificName { get; set; } = null!;
+
+    public string? CommonName { get; set; }
+
+    public string? NickName { get; set; }
+
+    public DateTime? DatePlanted { get; set; }
+
+    public string? Notes { get; set; }
+
+    public string? Sun { get; set; }
+
+    public DateTime? LastWater { get; set; }
+
+    public DateTime? LastFertilization { get; set; }
+
+    public bool? IsHealthy { get; set; }
+
+    public virtual UserInfo? User { get; set; }
+}
