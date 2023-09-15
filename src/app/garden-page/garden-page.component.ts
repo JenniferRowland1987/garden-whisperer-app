b65f86f0f3ebd8e9c9c7ab8673881b18bc0f390b
Denise <119461139+Denise1536@users.noreply.github.com>
2023-09-14 21:59:18 -0400

    some garden stuff

--- src/app/garden-page/garden-page.component.ts
+++ src/app/garden-page/garden-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GardenService } from '../garden.service';
 
 @Component({
   selector: 'app-garden-page',
   templateUrl: './garden-page.component.html',
   styleUrls: ['./garden-page.component.css']
 })
-export class GardenPageComponent {
+export class GardenPageComponent implements OnInit{
+
+  plants: any[] = [];
+
+  constructor(private gardenService:GardenService){}
+
+  ngOnInit(): void {
+    this.getPlants()
+  }
+
+getPlants(){
+  this.gardenService.getPlants(),subscribe((plants) => {
+    this.plants = plants;
+  });
+}
+
+getPlantsFromService() {
+  this.gardenService.getPlants().subscribe(
+    (data: any[]) => {
+    },
+    (error: any) => {
+      console.log(error);
+    }
+  );
+}
 
 }
+function subscribe(arg0: (plants: any) => void) {
+  throw new Error('Function not implemented.');
+}
+
