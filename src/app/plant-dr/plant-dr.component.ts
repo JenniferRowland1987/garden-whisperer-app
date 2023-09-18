c2fe925bd877287aeb5f28b0df48031b563e8d92
Jennifer <jenniferrowland1987@gmail.com>
2023-09-18 16:31:19 -0400

    9/18 update
    added in some stand in images, set up services for user log in and plant doctor, started typescript, html and css, will have to wait to test until we set up further routing.

--- src/app/plant-dr/plant-dr.component.ts
+++ src/app/plant-dr/plant-dr.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlantDrService } from '../plant-dr.service';
 
 @Component({
   selector: 'app-plant-dr',
   templateUrl: './plant-dr.component.html',
   styleUrls: ['./plant-dr.component.css']
 })
-export class PlantDrComponent {
+export class PlantDrComponent implements OnInit {
+  commonName: string = 'x';
+  pests: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute;
+    private plantDrService: PlantDrService;
+  ){}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const paramName = params.get('commonName');
+      if (paramName !== null) {
+        this.commonName = paramName; 
+        this.plantDrService.getPests(this.commonName).subscribe((data) => {
+          this.pests = data;
+        });
+      }
+    });
+  }
+
+  /*
+  From the plant details we will need to route with something like this:  
+ 
+  viewPlantPests(commonName: string) {
+    this.router.navigate(['/plantDr', commonName]);
+  }
+   */
+
 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private apiUrl = "https://localhost:7200/api/UserInfo"
+
+  public userID = 0
+
+  constructor(private http: HttpClient){}
+
+  getAllUsers(): Observable<any>
+  {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  getUser(id: number): Observable<any>
+  {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<any>(url);
+  } 
+
+  createUser(user: any)
+  {
+    return this.http.post<any>(this.apiUrl, user);
+  }
 }
