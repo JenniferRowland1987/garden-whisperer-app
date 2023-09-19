c1012528d3d9e55945b6661ceb180b8d76a542e1
GottaGinger <114538296+GottaGinger@users.noreply.github.com>
2023-09-18 20:50:00 -0400

    changes

--- src/app/search-plant/search-plant.component.ts
+++ src/app/search-plant/search-plant.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PlantService } from '../plant.service';
 
 @Component({
   selector: 'app-search-plant',
   templateUrl: './search-plant.component.html',
   styleUrls: ['./search-plant.component.css']
 })
-export class SearchPlantComponent {
+export class SearchPlantComponent implements OnInit {
+  commonName: string = "x"
+  plant: any[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private plantService: PlantService,
+  ){}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const paramName = params.get('commonName');
+      if (paramName !== null) {
+        this.commonName = paramName; 
+        this.plantService.getPlant(this.commonName).subscribe((data) => {
+          this.plant = data;
+        });
+      }
+    });
+  }
 }
