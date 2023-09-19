11c63493b6e6665839afc7be3058e35a6a9ad535
Denise <119461139+Denise1536@users.noreply.github.com>
2023-09-18 20:42:03 -0400

    2 more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { LoginComponent } from './login/login.component';
 import { PlantDetailsComponent } from './plant-details/plant-details.component';
 import { PlantDrComponent } from './plant-dr/plant-dr.component';
 import { WeatherComponent } from './weather/weather.component';
+import { PestDetailsComponent } from './pest-details/pest-details.component';
+import { SearchPlantComponent } from './search-plant/search-plant.component';
 
 
 const routes: Routes = [
@@ -19,6 +21,8 @@ const routes: Routes = [
   {path: 'plant-details', component: PlantDetailsComponent},
   {path: 'plant-dr', component: PlantDrComponent},
   {path: 'weather', component: WeatherComponent},
+  {path: 'pest-details', component: PestDetailsComponent},
+  {path: 'search-plant', component: SearchPlantComponent},
 
 ];
 
