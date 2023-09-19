df708753323d7837443e4e935a494f411e8bdd81
GottaGinger <114538296+GottaGinger@users.noreply.github.com>
2023-09-19 19:49:45 -0400

    updated components

--- src/app/plant-details/plant-details.component.ts
+++ src/app/plant-details/plant-details.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { PlantService } from '../plant.service';
+import { GardenService } from '../garden.service';
+import { PlantDrService } from '../plant-dr.service';
+
+
 
 @Component({
   selector: 'app-plant-details',
@@ -7,7 +11,30 @@ import { PlantService } from '../plant.service';
   styleUrls: ['./plant-details.component.css']
 })
 export class PlantDetailsComponent {
+
+  constructor(
+    private gardenService:GardenService,
+    private plantService: PlantService,
+    private plantDrService: PlantDrService
+    ){}
+    ngOnInit() {
+     
+      this.plantService.searchPlantsByCommon
+      this.gardenService.getPlant
+      this.plantDrService.getPests
+  
+    }
   //this pulls data from the api as well as from the added plant and route to the pest api -
   // this is where it will ask you if youre having problems
+}
+
+export class perenualPlant {
+  id: number = 0;
+  common_name: string = '';
+  scientific_name: string[] = [];
+  other_name: string[] = [];
+  cycle: string = '';
+  watering: string = '';
+  sunlight: string[] = [];
 
 }
