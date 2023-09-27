4b0beec20b6886a4f013f549a16e253797e8626b
Denise <119461139+Denise1536@users.noreply.github.com>
2023-09-27 19:16:46 -0400

    weather css

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  private apiUrl = "https://localhost:7200/api/UserInfo"
+  private apiUrl = "https://localhost:7200/api/UserInfo" 
 
   public userID = 2;
 
